e78e75c14eac97494dcfbc706293ed69e8a8f124
zsline <sergei.dereka@gmail.com>
2023-02-27 22:03:14 +0200

    advantage

--- src/js/app.js
+++ src/js/app.js
@@ -1,12 +1,31 @@
 import * as flsFunctions from "./modules/functions.js";
 import Swiper, { Navigation, Pagination } from 'swiper';
 
+
+
 flsFunctions.isWebp();
 
 
 const swiper = new Swiper('.offer-slider', {
+
     navigation: {
         nextEl: '.swiper-button-next',
         prevEl: '.swiper-button-prev',
-    }
+    },
 });
+
+const swiper2 = new Swiper('.portfolio__container', {
+    modules: [Navigation, Pagination],
+    centeredSlides: true,
+    loop: true,
+    slidesPerView: 2.6,
+    spaceBetween: 0,
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+    pagination: {
+        el: '.swiper-pagination',
+        type: 'bullets',
+    },
+})
